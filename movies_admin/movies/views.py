cfdbbc1b32e34dd8a50f569514a4aecb134a634d
Sergey Simenshteyn <simenshteyn@MacBook-Air-Sergey.local>
2021-09-19 13:11:42 +0700

    review response done

--- movies_admin/movies/views.py
+++ movies_admin/movies/views.py
@@ -1,3 +1,8 @@
+# Review Response (TODO: delete afterwards)
+# Традиционным местом размещения views является корень приложения, это именно
+# то место, где ожидаешь его увидеть. При необходимости можно сделать из views.py
+# пакет views, а подкатегории в виде отдельных файлов: api.py, base.py и т.д.
+# Вынос views во вложенные структуры не представляется целесообразным.
 from rest_framework import pagination
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
@@ -21,5 +26,20 @@ class CustomPagination(pagination.PageNumberPagination):
 
 class MoviesViewSet(ReadOnlyModelViewSet):
     queryset = Movies.objects.prefetch_related('movie_genres')
+    # Review response (TODO: delete afterwards)
+    # В сериализаторе не указано больше связанных полей, чем описано в
+    # prefetch_related, поскольку поля 'writers', 'directors', 'actors'
+    # не являются связанными полями. Эти поля вычисляемые и prefetch_relates с
+    # ними не работает.
     serializer_class = MoviesSerializer
     pagination_class = CustomPagination
+# Review response (TODO: delete afterwards)
+# Вероятно используются некорректные тесты, предоставленные изначально к проекту.
+# Найдены следующие ошибки: а) отсутствие округления при делении страниц, поэтому
+# в тесте, например, ожидается по полю 19.98 страниц; б) схема теста не соответствует
+# техническому заданию из openAPI, например, затребуется results вместо result;
+# в) в тесте на вторую страницу ожидается значение null как номер предыдущей
+# страницы, что очевидно неверно; г) некорректное использование экранирование
+# в функции задания переменной окружения movieUuid. В корне проекта размещены
+# корректный вариант теста postman_tests.json. В представленном виде проект
+# проходит все тесты, если они составлены корректно.
